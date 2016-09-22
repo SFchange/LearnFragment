105ddfd5781b2c3c0044dbc21674db3119728fca
SFchange <15737568028@163.com>
2016-09-22 18:56:39 +0800

    修改tabs

--- app/src/main/java/com/bianfeng/learnfragment/Tabs.java
+++ app/src/main/java/com/bianfeng/learnfragment/Tabs.java
@@ -58,7 +58,7 @@ public class Tabs extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action 替换自己的action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
